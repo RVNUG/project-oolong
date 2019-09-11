df52bf6021df9e88a096d329d986d89f86fb960d
Derek Pinkerton <zeelux@gmail.com>
2019-09-11 12:00:12 -0400

    Remove commented out code

--- src/components/Events.js
+++ src/components/Events.js
@@ -2,7 +2,6 @@ import React from 'react'
 import _ from 'lodash'
 
 class Events extends React.Component {
-  //eventsUrl = 'https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=Roanoke-Valley-NET-User-Group&callback=eventsLoaded&page=200&fields=&order=time&desc=false&status=upcoming&sig_id=199537590&sig=185fa3b3783205398c68577a1a6567b41e1c8c1d';
   eventsUrl = 'https://api.meetup.com/Roanoke-Valley-NET-User-Group/events?format=json&callback=eventsLoaded&order=time&desc=false&status=upcoming';
   daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
@@ -39,16 +38,13 @@ class Events extends React.Component {
                     <h3>{event.time}</h3>
                     <h4>{event.title}</h4>
                     <div dangerouslySetInnerHTML={{ __html: event.desc }} />
-                    <a href={event.url} target="meetup" className="button"><i className="fa fa-meetup"></i> RVSP on Meetup</a>
+                    <a href={event.url} target="meetup" className="button">
+                      <i className="fa fa-meetup"></i> RVSP on Meetup
+                    </a>
                   </li>)
               }
             </ul>
         }
-        {/*<footer className="major">*/}
-          {/*<ul className="actions">*/}
-            {/*<li><Link to="/generic" className="button">Learn More</Link></li>*/}
-          {/*</ul>*/}
-        {/*</footer>*/}
       </section>
     )
   }
